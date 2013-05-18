5e36c16f57ddcc058d5e77cae27f9d2875ff1b55
Steve Downey <steve.downtown@gmail.com>
2013-05-18 10:28:36 -0700

    doc for badge

--- app/helpers/bootstrap/badge_helper.rb
+++ app/helpers/bootstrap/badge_helper.rb
@@ -1,16 +1,13 @@
+# Rails helper for producing Twitter Bootstrap badges
+#
 # See: http://twitter.github.io/bootstrap/components.html#labels-badges
 # 
-# Helper for producing Twitter Bootstrap badges
-#
 # Default badge:
 #
+# @example
 #   badge('Default')
 #
-# Other badges (see BADGE_TYPES):
-#
 #   badge('Info', :info)
-#
-# Options passed through to <span> tag:
 #
 #   badge('Warning', :warning, id: 'warn-id', class: 'more-class', my_key: 'my_value')
 #
@@ -19,6 +16,19 @@ module Bootstrap::BadgeHelper
   
   BADGE_TYPES = %w(default success warning important info inverse)
   
+  # Returns html for a Bootstrap badge.
+  #
+  # @overload badge(text, options={})
+  #   Returns html for +default+ badge
+  #   @param [String] text Text of badge
+  #   @param [Hash] options Html attributes for badge <span>
+  # @overload badge(text, badge_type, options={})
+  #   Returns html for badge of type _badge_type_
+  #   @param [String] text Text of badge
+  #   @param [String, Symbol] badge_type one of BADGE_TYPES
+  #   @param [Hash] options Html attributes for badge <span>
+  # @return [String] html for badge
+  # @raise [InvalidBadgeTypeError] When a badge type is passed and isn't one of the BADGE_TYPES
   def badge(*args)
     text = args.shift
     options = add_badge_classes(*args)
