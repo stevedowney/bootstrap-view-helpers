5d387a302d41d9657127ead4e17b5859b28fa954
Steve Downey <steve.downtown@gmail.com>
2013-05-18 12:08:28 -0700

    document common

--- app/helpers/bootstrap/common_helper.rb
+++ app/helpers/bootstrap/common_helper.rb
@@ -1,6 +1,12 @@
+# Utililty methods used by Bootstrap::*Heler classes
 module Bootstrap::CommonHelper
   ArgumentError = Class.new(::ArgumentError)
   
+  # Returns a new Hash with:
+  # * keys converted to Symbols
+  # * the +:class+ key has it value converted to an Array of String
+  # @raise [ArgumentError] if _hash_ is not a Hash
+  # @return [Hash]
   def canonicalize_options(hash)
     raise ArgumentError.new("expected a Hash, got #{hash.inspect}") unless hash.is_a?(Hash)
 
@@ -10,10 +16,11 @@ module Bootstrap::CommonHelper
   end
 
   # Returns a new Array of String
-  # 
+  # @example
   #   arrayify_class_and_stringify_elements(nil)            #=> [] 
   #   arrayify_class_and_stringify_elements('foo')          #=> ["foo"]
   #   arrayify_class_and_stringify_elements([:foo, 'bar'])  #=> ["foo", "bar"]
+  # @return [Array of String]
   def arrayify_class_and_stringify_elements(klass)
     return false if klass == false
     
@@ -25,32 +32,42 @@ module Bootstrap::CommonHelper
   end
   
   # Returns down-caret character used in various dropdown menus.
+  # @param [Hash] options html options for <span>
+  # @example
+  #   caret(id: 'my-id')   #=> <span class='caret' id='my-id'></span>
+  # @return [String]
   def caret(options={})
     options= canonicalize_options(options)
     options = ensure_class(options, 'caret')
     content_tag(:span, nil, options)
   end
   
-  # Ensures that _hash_ has key of :class that includes _klass_.
+  # Returns new Hash where :class value includes _klasses_.
   # 
-  # Assumes _hash_ is a key :class (Symbol) whose value is an Array of String.
-  # #canonicalize_options will return such a Hash.
+  # Assumes _hash_ has a key :class (Symbol) whose value is an Array of String.
+  # {Bootstrap::CommonHelper#canonicalize_options} will return such a Hash.
   #
-  #   ensure_class({}, 'foo')                            #=> {class: 'foo'}
-  #   ensure_class({class: 'bar', id: 'my-id'}, 'foo')   #=> {:class=>["bar", "foo"], :id=>"my-id"}
-  def ensure_class(hash, klass)
+  # @example
+  #   ensure_class({class: []}, 'foo')                               #=> {class: 'foo'}
+  #   ensure_class({class: ['bar'], id: 'my-id'}, ['foo', 'foo2'])   #=> {:class=>["bar", "foo", "foo2"], :id=>"my-id"}
+  # @param [Hash] hash
+  # @param [String, Array] klasses one or more classes to add to the +:class+ key of _hash_
+  # @return [Hash]
+  def ensure_class(hash, klasses)
     hash.dup.tap do |h|
-      Array(klass).map(&:to_s).each do |k|
+      Array(klasses).map(&:to_s).each do |k|
         h[:class] << k unless h[:class].include?(k)
       end
     end
   end
 
   # Returns extra arguments that are Bootstrap modifiers.  Basically 2nd argument
-  # up to (not including) the last (hash) argument.
+  # up to (not including) the last (Hash) argument.
   #
+  # @example
   #   extract_extras('text')  #=> []
   #   extract_extras('text', :small, :info, id: 'foo')  #=> [:small, :info]
+  # @return [Array]
   def extract_extras(*args)
     args.extract_options!
     args.shift
